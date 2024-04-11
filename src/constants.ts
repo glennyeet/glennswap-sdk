09fb2de3901abf8e2e9b312b0993864dfbe2f396
Glenn Ye <lcm7063@gmail.com>
2024-04-11 15:57:06 -0400

    Add Sepolia contract addresses

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  SEPOLIA = 11155111
 }
 
 export enum TradeType {
@@ -22,9 +23,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xd1580edc20cc54ad3beb42a020187b3076b9bd62'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0xfffcdaa6ec3cc2a3623da1f1f40b1840403d07d2954ae0329467b137b1579a69'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
